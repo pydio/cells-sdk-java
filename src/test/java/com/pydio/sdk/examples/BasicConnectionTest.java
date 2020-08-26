4af607b1fd1ee9bf8eea254a04df6fcc13ead567
Bruno Sinou <bruno.sinou@posteo.de>
2020-08-26 18:43:09 +0200

    First PoC for future integration tests

--- src/test/java/com/pydio/sdk/examples/BasicConnectionTest.java
+++ src/test/java/com/pydio/sdk/examples/BasicConnectionTest.java
@@ -3,7 +3,6 @@ package com.pydio.sdk.examples;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 
 import com.pydio.sdk.core.model.ServerNode;
 import com.pydio.sdk.core.common.errors.Error;
@@ -13,48 +12,83 @@ import com.pydio.sdk.core.common.errors.SDKException;
 import com.pydio.sdk.core.model.Message;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
+/**
+ * Perform basic tests against a running Cells instance. You must first adapt config.properties files to your context
+ * and set the skipTest flag to false.
+ * 
+ * You can then launch the test with:
+ * 
+ * <code>gradle test --rerun-tasks  --tests com.pydio.sdk.examples.BasicConnectionTest -i</code>
+ */
 public class BasicConnectionTest {
 
-    private String serverURL = "http://localhost:8080";
-    private String login = "admin";
-    private String pwd = "admin";
-    private String workspace = "common-files";
-
     private ServerNode node;
+    private Client  cellsClient;
+
+    private Boolean skipTests = true;
+    private String serverURL, login, pwd, workspace;
 
     @Before
     public void setupThis() {
-        System.out.println("... Setting up");
 
-        node = new ServerNode();
-
-        Error error = node.resolve(serverURL);
-        if (error != null) {
-            System.out.println(error);
+        Properties p = new Properties();
+        try (InputStream is = BasicConnectionTest.class.getResourceAsStream("/config.properties")) {
+            p.load(new InputStreamReader(is));
+            skipTests = !("false".equals(p.getProperty("skipIntegrationTests")));
+            serverURL = p.getProperty("serverURL");
+            login = p.getProperty("login");
+            pwd = p.getProperty("pwd");
+            workspace = p.getProperty("defaultWorkspace");
+        } catch (IOException e) {
+            System.out.println("could not retrieve configuration file, cause: ");
+            System.out.println(e);
             return;
         }
 
+        if (skipTests){
+            System.out.println("... SkipTests flag is set => skipping BasicConnectionTest");
+            return;
+        }
+
+        node = new ServerNode();
+        Error error = node.resolve(serverURL);
+        if (error != null) {
+            System.out.println("Could not resolve server URL, cause: ");
+            System.out.println(error);
+        }
+
+        cellsClient = ClientFactory.get().Client(node);
+        cellsClient.setCredentials(new Credentials(login, pwd));
+        cellsClient.setSkipOAuthFlag(true);
     }
 
-    @Ignore("not yet ready , Please ignore.")
+    // @Ignore("not yet ready , Please ignore.")
     @Test
     public void method() {
 
-        System.out.println("... Launching test method");
+        if (skipTests){
+            return;
+        }
 
-        Client client = ClientFactory.get().Client(node);
-        client.setCredentials(new Credentials(login, pwd));
-        client.setSkipOAuthFlag(true);
+        System.out.println("... Test Listing");
+        try {
+            cellsClient.ls(workspace, "/", (node) -> System.out.println(node.label()));
+        } catch (SDKException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println("... Test Upload");
         String targetDir = "/"; // root
         String name = "hello6.txt";
-
         byte[] content = "Hello Pydio!".getBytes();
         ByteArrayInputStream source = new ByteArrayInputStream(content);
-
         try {
-            Message msg = client.upload(source, content.length, workspace, targetDir, name, true, (progress) -> {
+            Message msg = cellsClient.upload(source, content.length, workspace, targetDir, name, true, (progress) -> {
                 System.out.printf("\r%d bytes written\n", progress);
                 return false;
             });
@@ -62,10 +96,11 @@ public class BasicConnectionTest {
                 System.out.println("After upload, no message.");
             else
                 System.out.println("After upload, message: " + msg.message);
-
         } catch (SDKException e) {
             e.printStackTrace();
         }
+
+        // TODO finish implementing the CRUD and corresponding checks. Typically, for the time being upload fails silently.
     }
 
     @After
