00c2c57c06995d314e2a6f1d0555367b52556a45
Bruno Sinou <bruno.sinou@posteo.de>
2020-09-27 22:26:16 +0200

    Rather internally rely on an URL object

--- src/main/java/com/pydio/sdk/core/model/ServerNode.java
+++ src/main/java/com/pydio/sdk/core/model/ServerNode.java
@@ -7,7 +7,6 @@ import com.pydio.sdk.core.common.errors.Code;
 import com.pydio.sdk.core.common.errors.Error;
 import com.pydio.sdk.core.security.CertificateTrust;
 import com.pydio.sdk.core.security.CertificateTrustManager;
-import com.pydio.sdk.core.service.ServerResolution;
 import com.pydio.sdk.core.utils.io;
 
 import org.json.JSONObject;
@@ -17,8 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.cert.CertPathValidatorException;
 import java.security.cert.Certificate;
@@ -39,44 +38,61 @@ import javax.net.ssl.TrustManager;
 
 /**
  * Wraps a server properties
- * 
- * 
- * 
  */
 public class ServerNode implements Node {
 
     public Map<String, WorkspaceNode> workspaces;
-    private String scheme = null;
-    private String host = null;
-    private int port = 80;
-    private String path = null;
+
+    private URL serverURL = null;
+    private String originalUrl = null;
+
+    //    private String scheme = null;
+//    private String host = null;
+//    private int port = 80;
+//    private String path = null;
     private String version = null;
     private String versionName = null;
+
     private String iconURL;
     private String welcomeMessage;
     private String label = null;
-    private String url = null;
-    private String originalUrl = null;
-    private boolean sslUnverified = false;
-    private SSLContext sslContext;
-    private Properties properties = null;
-    private JSONObject bootConf, oidc;
+
+    //    private String url = null;
+    private JSONObject bootConf;
+    private JSONObject oidc;
+
+    @Deprecated
+    private ServerResolver serverResolver;
+
     private byte[][] certificateChain;
     private CertificateTrust.Helper trustHelper;
-    private ServerResolver serverResolver;
+    private boolean sslUnverified = false;
+    private SSLContext sslContext;
+
+    private Properties properties = null;
 
     public ServerNode() {
     }
 
-    public static ServerNode fromAddress(String address) throws IOException {
+    public ServerNode(URL url) {
+        this.serverURL = url;
+    }
+
+    public static ServerNode fromAddress(String address) throws MalformedURLException {
         URL url = new URL(address);
-        ServerNode node = new ServerNode();
-        node.scheme = url.getProtocol();
-        node.host = url.getHost();
-        node.port = url.getPort();
-        node.path = url.getPath();
-        node.url = address;
-        return node;
+        return new ServerNode(url);
+//        try {
+//            url
+//            ServerNode node = new ServerNode();
+//            node.scheme = url.getProtocol();
+//            node.host = url.getHost();
+//            node.port = url.getPort();
+//            node.path = url.getPath();
+//            node.url = address;
+//            return node;
+//        } catch (MalformedURLException mue){
+//            throw new MalformedURLException("Please provide a correct address",mue);
+//        }
     }
 
     // Node methods
@@ -85,6 +101,7 @@ public class ServerNode implements Node {
         if (properties == null) return null;
         return properties.getProperty(key);
     }
+
     @Override
     public void setProperty(String key, String value) {
         if (properties == null) {
@@ -92,45 +109,50 @@ public class ServerNode implements Node {
         }
         properties.setProperty(key, value);
     }
+
     @Override
     public void deleteProperty(String key) {
         if (properties != null && properties.contains(key)) {
             properties.remove(key);
         }
     }
-    @Override
-    public String path() {
-        return path;
-    }
+
     @Override
     public String label() {
         return label;
     }
+
     @Override
     public int type() {
         return Node.TYPE_SERVER;
     }
+
     @Override
     public String id() {
-        String id = scheme + "://" + host;
-        if (port != 80) {
-            id = id + ":" + port;
-        }
-        id = id + path;
-        return id;
+        return serverURL.toString();
+//        String id = scheme + "://" + host;
+//        if (port != 80) {
+//            id = id + ":" + port;
+//        }
+//        id = id + path;
+//        return id;
     }
+
     @Override
     public void setProperties(Properties p) {
         properties = p;
     }
+
     @Override
     public String getEncoded() {
         return null;
     }
+
     @Override
     public String getEncodedHash() {
         return null;
     }
+
     @Override
     public int compare(Node node) {
         return 0;
@@ -142,29 +164,34 @@ public class ServerNode implements Node {
     }
 
     private Error resolveRemote(final String address) {
-        originalUrl = address;
-        this.url = address;
-
-        if (!address.startsWith("https://") && !address.startsWith("http://")) {
-            try {
-                this.url = ServerResolution.resolve(address, true);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return new Error(Code.bad_uri);
-            }
-        }
-
-        URL url;
         try {
-            url = new URL(this.url);
-        } catch (MalformedURLException e) {
+            serverURL = new URL(address);
+        } catch (MalformedURLException mue) {
             return new Error(Code.bad_uri);
         }
 
-        scheme = url.getProtocol();
-        host = url.getHost();
-        port = url.getPort();
-        path = url.getPath();
+        originalUrl = address;
+
+//        if (!address.startsWith("https://") && !address.startsWith("http://")) {
+//            try {
+//                this.url = ServerResolution.resolve(address, true);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                return new Error(Code.bad_uri);
+//            }
+//        }
+
+//        URL url;
+//        try {
+//            url = new URL(this.url);
+//        } catch (MalformedURLException e) {
+//            return new Error(Code.bad_uri);
+//        }
+
+//        scheme = url.getProtocol();
+//        host = url.getHost();
+//        port = url.getPort();
+//        path = url.getPath();
 
         int err = downloadBootConf("a/frontend/bootconf");
         if (err == 0) {
@@ -175,8 +202,10 @@ public class ServerNode implements Node {
         if (err != Code.unexpected_response) {
             err = downloadBootConf("index.php?get_action=get_boot_conf");
             if (err == 0) {
+                // FIXME should re-enable using a P8 server backend
+                return new Error(Code.pydio_server_not_supported);
                 //c.onComplete(new Error(Pydio.ERROR_PYDIO_8_SERVER, "found pydio 8 server", null));
-                return null;
+                // return null;
             }
         }
         return new Error(err);
@@ -265,7 +294,7 @@ public class ServerNode implements Node {
         }
 
         try {
-            bootConf = new JSONObject(new String(out.toByteArray(), "UTF-8"));
+            bootConf = new JSONObject(new String(out.toByteArray(), StandardCharsets.UTF_8));
         } catch (Exception ignored) {
             return Code.unexpected_response;
         }
@@ -370,13 +399,12 @@ public class ServerNode implements Node {
         }
 
         try {
-            oidc = new JSONObject(new String(out.toByteArray(), "UTF-8"));
+            oidc = new JSONObject(new String(out.toByteArray(), StandardCharsets.UTF_8));
         } catch (Exception ignored) {
         }
     }
 
     // Getters
-
     private TrustManager trustManager() {
         return new CertificateTrustManager(getTrustHelper());
     }
@@ -431,16 +459,24 @@ public class ServerNode implements Node {
         return trustHelper;
     }
 
+    @Deprecated(forRemoval = true)
     public ServerNode init(String url) {
-        if (!url.endsWith("/")) {
-            url += "/";
+        try {
+            serverURL = new URL(url);
+        } catch (MalformedURLException mue) {
+            // TODO handle error cleanly
+            mue.printStackTrace();
         }
-        this.url = url;
-        URI uri = URI.create(url);
-        scheme = uri.getScheme();
-        host = uri.getHost();
-        path = uri.getPath();
-        port = uri.getPort();
+
+//        if (!url.endsWith("/")) {
+//            url += "/";
+//        }
+//        this.url = url;
+//        URI uri = URI.create(url);
+//        scheme = uri.getScheme();
+//        host = uri.getHost();
+//        path = uri.getPath();
+//        port = uri.getPort();
         return this;
     }
 
@@ -448,38 +484,48 @@ public class ServerNode implements Node {
         return sslUnverified;
     }
 
-    public String host() {
-        return host;
-    }
-
-    public String scheme() {
-        return scheme;
-    }
-
-    public int port() {
-        return port;
-    }
-
     public String iconURL() {
         return iconURL;
     }
 
     public String url() {
-        if (url != null) return url;
-        String path = scheme.toLowerCase() + "://" + host;
-        if (port > 0 && port != 80) {
-            path += ":" + port;
-        }
-        path += path();
-        if (!path.endsWith("/")) {
-            return path + "/";
-        }
-        return url = path;
+        return serverURL.toString();
+//        if (url != null) return url;
+//        String path = scheme.toLowerCase() + "://" + host;
+//        if (port > 0 && port != 80) {
+//            path += ":" + port;
+//        }
+//        path += path();
+//        if (!path.endsWith("/")) {
+//            return path + "/";
+//        }
+//        return url = path;
+    }
+
+
+    public String scheme() {
+        return serverURL.getProtocol();
+        // scheme
+    }
+
+    public String host() {
+        return serverURL.getHost();
+        // return host;
+    }
+
+    public int port() {
+        return serverURL.getPort();
+        //return port;
+    }
+
+    @Override
+    public String path() {
+        return serverURL.getPath();
     }
 
     public String getOriginalURL() {
-        if( originalUrl == null) {
-            originalUrl = url();
+        if (originalUrl == null) {
+            originalUrl = serverURL.toString();
         }
         return originalUrl;
     }
@@ -517,14 +563,6 @@ public class ServerNode implements Node {
         return sslContext;
     }
 
-    public boolean equals(Object o) {
-        try {
-            return this == o || (o instanceof Node) && ((Node) o).type() == type() && label().equals(((Node) o).label()) && path().equals(((Node) o).path());
-        } catch (NullPointerException e) {
-            return false;
-        }
-    }
-
     public WorkspaceNode getWorkspace(String slug) {
         if (workspaces != null && workspaces.containsKey(slug)) {
             return workspaces.get(slug);
@@ -541,7 +579,7 @@ public class ServerNode implements Node {
             return null;
         }
 
-        for (WorkspaceNode wn: workspaces.values()) {
+        for (WorkspaceNode wn : workspaces.values()) {
             if (id.equals(wn.getProperty(Pydio.WORKSPACE_PROPERTY_ID))) {
                 return wn;
             }
@@ -566,6 +604,7 @@ public class ServerNode implements Node {
         return (s, sslSession) -> true;
     }
 
+    @Deprecated
     public ServerResolver getServerResolver() {
         return this.serverResolver;
     }
@@ -593,5 +632,13 @@ public class ServerNode implements Node {
             this.workspaces.put(wn.getId(), wn);
         }
         return this;
+    }
+
+    public boolean equals(Object o) {
+        try {
+            return this == o || (o instanceof Node) && ((Node) o).type() == type() && label().equals(((Node) o).label()) && path().equals(((Node) o).path());
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 }
