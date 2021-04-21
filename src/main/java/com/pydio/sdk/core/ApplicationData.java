044f18f03889adb27e94ca937d2d0ac87b1413e7
jabar <jabar@ajaxplorer.info>
2021-04-21 18:50:37 +0200

    Set default values

--- src/main/java/com/pydio/sdk/core/ApplicationData.java
+++ src/main/java/com/pydio/sdk/core/ApplicationData.java
@@ -4,8 +4,9 @@ import com.pydio.sdk.core.common.callback.PersistentDataManager;
 
 public class ApplicationData {
 
-    public static String name = "";
-    public static String version = "";
+    public static String name = "Cells SDK Java";
+    public static String version = "0.1";
+    public static String packageID = "";
     public static int versionCode = 1;
 
     private static PersistentDataManager dataManager;
