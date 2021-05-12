f6370bcfed2b10820f8b6b759c91f2ca4bca8b42
jabarkarim <jabar@ajaxplorer.info>
2021-05-12 12:37:09 +0000

    Added StateID encoding/decoding tests

--- src/test/java/com/pydio/cells/transport/StateIDTest.java
+++ src/test/java/com/pydio/cells/transport/StateIDTest.java
@@ -1,4 +1,39 @@
 package com.pydio.cells.transport;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class StateIDTest {
+
+    @Before
+    public void setup() {}
+
+    @After
+    public void teardown() {}
+
+    @Test
+    public void testID() {
+        StateID stateID = new StateID("doe@gmail.com", "https://doefiles.com");
+        String encodedID = stateID.getId();
+
+        StateID decodedStateID = StateID.fromId(encodedID);
+
+        Assert.assertEquals(stateID.getUsername(), decodedStateID.getUsername());
+        Assert.assertEquals(stateID.getServerUrl(), decodedStateID.getServerUrl());
+    }
+
+    @Test
+    public void testIDWithPath() {
+        StateID stateID = new StateID("doe@gmail.com", "https://doefiles.com", "/common-files/Documents");
+        String encodedID = stateID.getId();
+
+        StateID decodedStateID = StateID.fromId(encodedID);
+
+        Assert.assertEquals(stateID.getUsername(), decodedStateID.getUsername());
+        Assert.assertEquals(stateID.getServerUrl(), decodedStateID.getServerUrl());
+        Assert.assertEquals(stateID.getFile(), decodedStateID.getFile());
+    }
 }
