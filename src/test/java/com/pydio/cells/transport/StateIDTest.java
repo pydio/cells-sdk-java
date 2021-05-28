7500057784da397aab782df04ed05407ba2acc67
Bruno Sinou <bruno.sinou@posteo.de>
2021-05-28 08:40:46 +0200

    Add Unit Test that breaks current implementation

--- src/test/java/com/pydio/cells/transport/StateIDTest.java
+++ src/test/java/com/pydio/cells/transport/StateIDTest.java
@@ -37,4 +37,29 @@ public class StateIDTest {
         Assert.assertEquals(stateID.getServerUrl(), decodedStateID.getServerUrl());
         Assert.assertEquals(stateID.getFile(), decodedStateID.getFile());
     }
+
+    @Test
+    public void testIDWithSpaceInPath() {
+        StateID stateID = new StateID("doe@gmail.com", "http://doefiles.com:6767", "/common-files/Sample Documents to test.../Broken Names/IMG-20201118-WA0001.jpg");
+        String encodedID = stateID.getId();
+
+        // https%3A%2F%2Fandroid21.ci.pyd.io%2Fcommon-files%2FSample+Documents+to+test...%2FBroken+Names%2FIMG-20201118-WA0001.jpg
+
+        StateID decodedStateID = StateID.fromId(encodedID);
+
+        Assert.assertEquals("doe@gmail.com", decodedStateID.getUsername());
+        Assert.assertEquals("http://doefiles.com:6767", decodedStateID.getServerUrl());
+        Assert.assertEquals("/common-files/Sample Documents to test.../Broken Names/IMG-20201118-WA0001.jpg", decodedStateID.getPath());
+        Assert.assertEquals("common-files", decodedStateID.getWorkspace());
+        Assert.assertEquals("/Sample Documents to test.../Broken Names/IMG-20201118-WA0001.jpg", decodedStateID.getFile());
+
+
+        Assert.assertEquals(stateID.getUsername(), decodedStateID.getUsername());
+        Assert.assertEquals(stateID.getServerUrl(), decodedStateID.getServerUrl());
+        Assert.assertEquals(stateID.getFile(), decodedStateID.getFile());
+    }
+
+
+
+
 }
