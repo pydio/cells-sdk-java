0b055c5334ec59f491742f82246b08301752abf1
Bruno Sinou <bruno.sinou@posteo.de>
2021-11-16 09:29:30 +0100

    Use standard sample data

--- src/test/java/com/pydio/cells/transport/StateIDTest.java
+++ src/test/java/com/pydio/cells/transport/StateIDTest.java
@@ -7,13 +7,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Basic tests to insure encoding / decoding of states work as expected.
+ * Basic tests to insure encoding/decoding of states work as expected.
  */
 public class StateIDTest {
 
     @Test
     public void testID() {
-        StateID stateID = new StateID("doe@gmail.com", "https://doefiles.com");
+        StateID stateID = new StateID("doe@example.com", "https://example.com");
         String encodedID = stateID.getId();
 
         StateID decodedStateID = StateID.fromId(encodedID);
@@ -24,13 +24,13 @@ public class StateIDTest {
 
     @Test
     public void testIDWithPath() {
-        StateID stateID = new StateID("doe@gmail.com", "http://doefiles.com:6767", "/common-files/Documents/Subfolder");
+        StateID stateID = new StateID("doe@example.com", "http://example.com:6767", "/common-files/Documents/Subfolder");
         String encodedID = stateID.getId();
 
         StateID decodedStateID = StateID.fromId(encodedID);
 
-        Assert.assertEquals("doe@gmail.com", decodedStateID.getUsername());
-        Assert.assertEquals("http://doefiles.com:6767", decodedStateID.getServerUrl());
+        Assert.assertEquals("doe@example.com", decodedStateID.getUsername());
+        Assert.assertEquals("http://example.com:6767", decodedStateID.getServerUrl());
         Assert.assertEquals("/common-files/Documents/Subfolder", decodedStateID.getPath());
         Assert.assertEquals("common-files", decodedStateID.getWorkspace());
         Assert.assertEquals("/Documents/Subfolder", decodedStateID.getFile());
@@ -47,13 +47,13 @@ public class StateIDTest {
         String parPath = "/Sample Documents to test.../Broken Names";
         String name = "Ô my $$… & ¼ !! @ https:breaking.com yeßßß #3.jpg";
         String breakingPath = "/" + slug + parPath + "/" + name;
-        StateID stateID = new StateID("doe@gmail.com", "http://doefiles.com:6767", breakingPath );
+        StateID stateID = new StateID("doe@example.com", "http://example.com:6767", breakingPath );
         String encodedID = stateID.getId();
 
         StateID decodedStateID = StateID.fromId(encodedID);
 
-        Assert.assertEquals("doe@gmail.com", decodedStateID.getUsername());
-        Assert.assertEquals("http://doefiles.com:6767", decodedStateID.getServerUrl());
+        Assert.assertEquals("doe@example.com", decodedStateID.getUsername());
+        Assert.assertEquals("http://example.com:6767", decodedStateID.getServerUrl());
         Assert.assertEquals(breakingPath, decodedStateID.getPath());
         Assert.assertEquals(slug, decodedStateID.getWorkspace());
         Assert.assertEquals(parPath + "/" + name, decodedStateID.getFile());
@@ -64,18 +64,18 @@ public class StateIDTest {
     }
 
     @Test
-    public void testIDWithUncomplete() {
+    public void testIDWithIncomplete() {
 
         CustomEncoder encoder = new JavaCustomEncoder();
 
-        String url = "http://doefiles.com:6767";
+        String url = "http://example.com:6767";
         StateID stateID = StateID.fromId(encoder.utf8Encode(url));
 
         Assert.assertNull(stateID.getUsername());
         Assert.assertNull(stateID.getPath());
         Assert.assertEquals(url, stateID.getServerUrl());
 
-        String userName = "john@carter.com";
+        String userName = "john@example.com";
         stateID = StateID.fromId(encoder.utf8Encode(userName).concat("@").concat(encoder.utf8Encode(url)));
 
         Assert.assertEquals(userName, stateID.getUsername());
